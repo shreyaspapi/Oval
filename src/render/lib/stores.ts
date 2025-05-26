af2cb68e8de0f0bd3e11c7a74bd5f19a9e3b9023
Timothy Jaeryang Baek <tim@openwebui.com>
2025-05-26 20:00:49 +0400

    reboot

--- src/render/lib/stores.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const installStatus = writable(null);
-export const serverStatus = writable(null);
-
-export const serverStartedAt = writable(null);
-
-export const serverLogs = writable([]);
